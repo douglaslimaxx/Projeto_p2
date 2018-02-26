cea04a59f13a27ccf553a3bb2b98982477e462d6
Douglas Lima <douglaslima909@gmail.com>
2018-02-25 22:50:53 -0300

    Adicionei exceções no construtor de Tutor e criei a classe ControllerTutor

---
+++ src/models/ControllerTutor.java
@@ -0,0 +1,8 @@
+package models;
+
+public class ControllerTutor {
+
+	private Tutor tutor;
+	
+	
+}
--- src/models/Tutor.java
+++ src/models/Tutor.java
@@ -8,11 +8,53 @@ public class Tutor {
 	private int quantia;
 	
 	public Tutor(String disciplina, int proficiencia) {
+		if (disciplina == null) {
+			throw new NullPointerException();
+		}
+		if (disciplina.trim().equals("")) {
+			throw new IllegalArgumentException();
+		}
+		if (proficiencia <= 0) {
+			throw new ArithmeticException("Erro na definicao de papel: Proficiencia invalida");
+		}
 		this.disciplina = disciplina;
 		this.proficiencia = proficiencia;
 		this.avaliacao = 4;
 		this.quantia = 0;
 	}
+
+	public String getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public int getProficiencia() {
+		return proficiencia;
+	}
+
+	public void setProficiencia(int proficiencia) {
+		this.proficiencia = proficiencia;
+	}
+
+	public int getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(int avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public int getQuantia() {
+		return quantia;
+	}
+
+	public void setQuantia(int quantia) {
+		this.quantia = quantia;
+	}
+	
 	
 	
 	
