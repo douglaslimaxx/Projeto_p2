a70764b6694144083bbc1195bf5c4d167d2221f7
Douglas Lima <douglaslima909@gmail.com>
2018-02-25 21:06:29 -0300

    Criei a classe Tutor, com os atributos e o construtor

---
+++ src/models/Tutor.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Tutor {
+
+	private String disciplina;
+	private int proficiencia;
+	private int avaliacao;
+	private int quantia;
+	
+	public Tutor(String disciplina, int proficiencia) {
+		this.disciplina = disciplina;
+		this.proficiencia = proficiencia;
+		this.avaliacao = 4;
+		this.quantia = 0;
+	}
+	
+	
+	
+}
