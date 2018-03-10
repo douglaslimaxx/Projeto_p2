47b48e790e98288d9649fd96d6204813a052d44b
Marta Laís <marta.dantas@ccc.ufcg.edu.br>
2018-03-10 14:00:16 -0300

    Documentação da classe MatriculaComparator.java

--- src/models/MatriculaComparador.java
+++ src/models/MatriculaComparador.java
@@ -3,12 +3,16 @@ package models;
 import java.util.Comparator;
 
 /**
- * 
+ * Classe que compara a matricula dos alunos, utilizando o comparator (compareTo).
  * @author Douglas Lima
- *
+ * 
  */
 public class MatriculaComparador implements Comparator<Aluno>{
 
+	/**
+	 * Metodo que compara a matricula de dois alunos.
+	 * @return
+	 */
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		return o1.getMatricula().compareTo(o2.getMatricula());
