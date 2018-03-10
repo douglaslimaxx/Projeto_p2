b0d3caff8a7a352901d585c917baf7536f09887b
Marta Laís <marta.dantas@ccc.ufcg.edu.br>
2018-03-10 14:07:09 -0300

    Documentação da classe NomeComparador.java

--- src/models/NomeComparador.java
+++ src/models/NomeComparador.java
@@ -3,20 +3,23 @@ package models;
 import java.util.Comparator;
 
 /**
- * 
+ * Classe que compara os os nomes dos alunos, utilizando o comparator (compareTo).
  * @author Douglas Lima
- *
+ * 
  */
 public class NomeComparador implements Comparator<Aluno>{
 
+	/**
+	 * Metodo que compara os nomes dos alunos.
+	 * @return
+	 * 
+	 */
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		if (o1.getNome().compareTo(o2.getNome()) != 0) {
 			return o1.getNome().compareTo(o2.getNome());
 		} else {
 			return o1.getMatricula().compareTo(o2.getMatricula());
-		}
-		
+		}	
 	}
-
 }
