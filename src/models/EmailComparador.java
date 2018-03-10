1b9b8b25b3402e297f11c9b3e7a7cd93c5db0fa1
Marta Laís <marta.dantas@ccc.ufcg.edu.br>
2018-03-10 14:05:22 -0300

    Documentação da classe EmailComparador.java

--- src/models/EmailComparador.java
+++ src/models/EmailComparador.java
@@ -3,12 +3,17 @@ package models;
 import java.util.Comparator;
 
 /**
- * 
+ * Classe que compara os emails dos alunos, utilizando o comparator (compareTo).
  * @author Douglas Lima
- *
+ * 
  */
 public class EmailComparador implements Comparator<Aluno>{
 
+	/**
+	 * Metodo que compara os emails dos alunos.
+	 * @return
+	 * 
+	 */
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		if (o1.getEmail().compareTo(o2.getMatricula()) != 0) {
